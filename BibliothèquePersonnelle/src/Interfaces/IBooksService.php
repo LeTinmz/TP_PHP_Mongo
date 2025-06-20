7176c8590f628587ed0e18ce64696d30aa3156cd
Tinmar <martin.lecerf@hotmail.fr>
2025-06-20 14:09:27 +0200

    merge test

--- BibliothèquePersonnelle/src/Interfaces/IBooksService.php
+++ BibliothèquePersonnelle/src/Interfaces/IBooksService.php
@@ -2,21 +2,22 @@
 
 <?php
 
+use App\Enum\CategoryType;
+
 interface IBooksService
 {
     public function addBook(BookDTO $book);
 
-    public function editBook($id);
+    public function editBook(int $id, BookDTO $book);
 
-    public function deleteBook(BookDTO $book);
+    public function deleteBook(int $id);
 
     public function getAllBooks():array /* Tableau de BookDTO*/;
 
     public function getBookById(int $id):BookDTO;
 
-    public function getBooksByCategory(\App\Enum\CategoryType $category):array /* Tableau de BookDTO*/;
+    public function getBooksByCategory(CategoryType $category):array /* Tableau de BookDTO*/;
 
     public function getBookRatingByBookId(int $id);
-
 
 }
